7acb7340362c1912c436ad050265ffbd8230aa20
mirsella <mirsella@protonmail.com>
2023-07-07 15:19:25 +0200

    Update .config/zsh/lib/zprofile.zsh

--- private_dot_config/zsh/lib/executable_zprofile.zsh
+++ private_dot_config/zsh/lib/executable_zprofile.zsh
@@ -101,9 +101,9 @@ fi
 '
 
 # zsh auto notify
-export AUTO_NOTIFY_THRESHOLD=20
-export AUTO_NOTIFY_TITLE="%command"
-export AUTO_NOTIFY_BODY="%elapsed sec\n%exit_code"
+export AUTO_NOTIFY_THRESHOLD=15
+export AUTO_NOTIFY_TITLE="code: %exit_code, took %elapsed sec"
+export AUTO_NOTIFY_BODY="%command"
 export AUTO_NOTIFY_EXPIRE_TIME=2000
 AUTO_NOTIFY_IGNORE+=("less" "more" "man" "tig" "watch" "git commit" "top" "htop" "nano" "v" "nvim" "s nvim -p" "arduino-cli")
 
