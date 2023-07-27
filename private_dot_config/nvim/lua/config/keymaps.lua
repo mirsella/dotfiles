47d962a5f8f065456e98dc87a1c6b69edf2935e6
mirsella <mirsella@protonmail.com>
2023-07-28 01:13:50 +0200

    Update .chezmoiexternal.toml Add .config/nvim/.neoconf.json Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lua/config/autocmds.lua Add .config/nvim/lua/config/keymaps.lua Add .config/nvim/lua/config/lazy.lua Add .config/nvim/lua/config/options.lua Add .config/nvim/lua/plugins/example.lua Add .config/nvim/stylua.toml

---
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,7 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.api.nvim_create_user_command("W", "w !sudo tee %", {})
+
+vim.keymap.set("n", "U", "<cmd>echo '< < ===== C H E C K   C A P S   L O C K ===== > >'<cr>")
