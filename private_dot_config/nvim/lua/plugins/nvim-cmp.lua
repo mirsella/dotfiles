f36a0b939dd06e768bdb696e86cf5337c64e8b8d
mirsella <mirsella@protonmail.com>
2023-07-28 17:59:43 +0200

    Remove .config/nvim/.neoconf.json Update .config/nvim/lua/config/lazy.lua Update .config/nvim/lua/plugins/nvim-cmp.lua Remove .config/nvim/stylua.toml Update .config/zsh/.gitignore

--- private_dot_config/nvim/lua/plugins/nvim-cmp.lua
+++ private_dot_config/nvim/lua/plugins/nvim-cmp.lua
@@ -3,16 +3,26 @@ return {
 	dependencies = {
 		{
 			"petertriho/cmp-git",
-			dependencies = { "nvim-lua/plenary.nvim" },
 			config = function()
 				require("cmp_git").setup()
 			end,
 		},
+		{
+			"jcdickinson/codeium.nvim",
+			commit = "b1ff0d6c993e3d87a4362d2ccd6c660f7444599f", -- see https://github.com/jcdickinson/codeium.nvim/pull/74
+			lazy = false,
+			config = function()
+				require("codeium").setup()
+			end,
+		},
+		"nvim-lua/plenary.nvim",
 	},
 	opts = function(_, opts)
 		local cmp = require("cmp")
 		opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
 			{ name = "git" },
+			{ name = "codeium" },
+			{ name = "copilot" },
 		}))
 	end,
 }
