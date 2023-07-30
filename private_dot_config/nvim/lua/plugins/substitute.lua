6878ac763d6a572507d02a2e0a3e70f04dae4231
mirsella <mirsella@protonmail.com>
2023-07-30 10:19:47 +0200

    Update .config/nvim/lua/plugins/substitute.lua

--- private_dot_config/nvim/lua/plugins/substitute.lua
+++ private_dot_config/nvim/lua/plugins/substitute.lua
@@ -1,13 +1,15 @@
 return {
 	"gbprod/substitute.nvim",
 	dependencies = { "gbprod/yanky.nvim" },
-	keys = {
-		{ "n", "gs", require("substitute").operator, { noremap = true, desc = "Substitute operator" } },
-		{ "n", "gss", require("substitute").line, { noremap = true, desc = "Substitute line" } },
-		{ "n", "gS", require("substitute").eol, { noremap = true, desc = "Substitute to end of line" } },
-		{ "x", "gs", require("substitute").visual, { noremap = true, desc = "Substitute visual selection" } },
-	},
+	event = "VeryLazy",
 	opts = {
 		on_substitute = require("yanky.integration").substitute(),
 	},
+	config = function(_, opts)
+		require("substitute").setup(opts)
+		vim.keymap.set("n", "gs", require("substitute").operator, { noremap = true, desc = "Substitute operator" })
+		vim.keymap.set("n", "gss", require("substitute").line, { noremap = true, desc = "Substitute line" })
+		vim.keymap.set("n", "gS", require("substitute").eol, { noremap = true, desc = "Substitute to end of line" })
+		vim.keymap.set("x", "gs", require("substitute").visual, { noremap = true, desc = "Substitute visual" })
+	end,
 }
