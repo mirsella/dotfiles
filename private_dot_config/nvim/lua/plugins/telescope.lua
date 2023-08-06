29d1c5e4293f34e9d6a72c85822138ffb087568c
mirsella <mirsella@protonmail.com>
2023-08-06 22:00:20 +0200

    Update .config/nvim/lua/plugins/telescope.lua

--- private_dot_config/nvim/lua/plugins/telescope.lua
+++ private_dot_config/nvim/lua/plugins/telescope.lua
@@ -4,6 +4,9 @@ return {
 		"nvim-lua/plenary.nvim",
 		"debugloop/telescope-undo.nvim",
 	},
+	keys = {
+		{ "<leader>st", false },
+	},
 	config = function()
 		require("telescope").load_extension("undo")
 		vim.keymap.set("n", "<leader>uu", "<cmd>Telescope undo<cr>", { desc = "Telescope undo" })
