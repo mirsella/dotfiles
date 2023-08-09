5122fc23de93ffeeea189ba4abacc7d8583ee9ab
mirsella <mirsella@protonmail.com>
2023-08-09 23:02:17 +0200

    Update .config/nvim/lua/plugins/telescope.lua

--- private_dot_config/nvim/lua/plugins/telescope.lua
+++ private_dot_config/nvim/lua/plugins/telescope.lua
@@ -1,11 +1,20 @@
+local Util = require("lazyvim.util")
+
 return {
-	"nvim-telescope/telescope.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"debugloop/telescope-undo.nvim",
-	},
-	config = function()
-		require("telescope").load_extension("undo")
-		vim.keymap.set("n", "<leader>uu", "<cmd>Telescope undo<cr>", { desc = "Telescope undo" })
-	end,
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "debugloop/telescope-undo.nvim",
+  },
+  keys = {
+    { "<leader>uu", "<cmd>Telescope undo<cr>", desc = "Telescope undo" },
+
+    { "<leader><space>", Util.telescope("files", { hidden = true }), desc = "Find Files (root dir)" },
+    { "<leader>ff", Util.telescope("files", { hidden = true }), desc = "Find Files (root dir)" },
+    { "<leader>fF", Util.telescope("files", { cwd = false, hidden = true }), desc = "Find Files (cwd)" },
+  },
+  config = function()
+    local telescope = require("telescope")
+    telescope.load_extension("undo")
+  end,
 }
