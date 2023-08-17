36aaa62f72679a0bdfe08fd3c31e43d224d2ed40
Lucas <mirsella@protonmail.com>
2023-08-17 10:56:56 +0200

    Update options.lua

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -6,5 +6,5 @@ vim.opt.pumblend = 0 -- opaque completion menu
 vim.opt.softtabstop = 2
 
 vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 vim.opt.foldlevel = 99
