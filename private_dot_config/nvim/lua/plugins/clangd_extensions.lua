af543a448e56b648062fd67cd203d29691074e0a
mirsella <mirsella@protonmail.com>
2023-08-30 21:30:22 +0200

    Update .config/nvim/lua/plugins/clangd_extensions.lua

--- private_dot_config/nvim/lua/plugins/clangd_extensions.lua
+++ private_dot_config/nvim/lua/plugins/clangd_extensions.lua
@@ -1,10 +1,8 @@
 return {
   "p00f/clangd_extensions.nvim",
   opts = {
-    extensions = {
-      inlay_hints = {
-        inline = true,
-      },
+    inlay_hints = {
+      inline = true,
     },
   },
 }
