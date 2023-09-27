5331a77cb195ea487e720509cf0860a57f9d92e2
mirsella <mirsella@protonmail.com>
2023-09-27 14:57:06 +0200

    Update .config/nvim/lua/config/keymaps.lua Update .config/nvim/lua/config/lazy.lua

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -9,21 +9,60 @@ vim.keymap.set("i", "<c-t>", "<esc>")
 vim.keymap.set("n", "<leader>sf", "<cmd>Telescope filetypes<cr>")
 
 local crates = require("crates")
-local opts = { silent = true }
 
-vim.keymap.set("n", "<leader>cv", crates.show_versions_popup, opts)
-vim.keymap.set("n", "<leader>cf", crates.show_features_popup, opts)
-vim.keymap.set("n", "<leader>cd", crates.show_dependencies_popup, opts)
+vim.keymap.set(
+	"n",
+	"<leader>cv",
+	crates.show_versions_popup,
+	{ silent = true, noremap = true, desc = "show crates versions" }
+)
+vim.keymap.set(
+	"n",
+	"<leader>cf",
+	crates.show_features_popup,
+	{ silent = true, noremap = true, desc = "show crates features" }
+)
+vim.keymap.set(
+	"n",
+	"<leader>cd",
+	crates.show_dependencies_popup,
+	{ silent = true, noremap = true, desc = "show crates dependencies" }
+)
 
-vim.keymap.set("n", "<leader>cu", crates.update_crate, opts)
-vim.keymap.set("v", "<leader>cu", crates.update_crates, opts)
-vim.keymap.set("n", "<leader>cU", crates.upgrade_crate, opts)
-vim.keymap.set("v", "<leader>cU", crates.upgrade_crates, opts)
+vim.keymap.set("n", "<leader>cu", crates.update_crate, { silent = true, noremap = true, desc = "update crate" })
+vim.keymap.set("v", "<leader>cu", crates.update_crates, { silent = true, noremap = true, desc = "update crates" })
+vim.keymap.set("n", "<leader>cU", crates.upgrade_crate, { silent = true, noremap = true, desc = "upgrade crate" })
+vim.keymap.set("v", "<leader>cU", crates.upgrade_crates, { silent = true, noremap = true, desc = "upgrade crates" })
 
-vim.keymap.set("n", "<leader>ce", crates.expand_plain_crate_to_inline_table, opts)
-vim.keymap.set("n", "<leader>cE", crates.extract_crate_into_table, opts)
+vim.keymap.set(
+	"n",
+	"<leader>ce",
+	crates.expand_plain_crate_to_inline_table,
+	{ silent = true, noremap = true, desc = "expand crate to inline table" }
+)
+vim.keymap.set(
+	"n",
+	"<leader>cE",
+	crates.extract_crate_into_table,
+	{ silent = true, noremap = true, desc = "expand crate into table" }
+)
 
-vim.keymap.set("n", "<leader>cH", crates.open_homepage, opts)
-vim.keymap.set("n", "<leader>cR", crates.open_repository, opts)
-vim.keymap.set("n", "<leader>cD", crates.open_documentation, opts)
-vim.keymap.set("n", "<leader>cC", crates.open_crates_io, opts)
+vim.keymap.set("n", "<leader>cH", crates.open_homepage, { silent = true, noremap = true, desc = "open crate homepage" })
+vim.keymap.set(
+	"n",
+	"<leader>cR",
+	crates.open_repository,
+	{ silent = true, noremap = true, desc = "open crate repository" }
+)
+vim.keymap.set(
+	"n",
+	"<leader>cD",
+	crates.open_documentation,
+	{ silent = true, noremap = true, desc = "open crate homepage" }
+)
+vim.keymap.set(
+	"n",
+	"<leader>cC",
+	crates.open_crates_io,
+	{ silent = true, noremap = true, desc = "open crate on crates.io" }
+)
