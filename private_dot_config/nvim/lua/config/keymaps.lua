f862c892722826e7408fe611c0d1a1bdc96d8945
mirsella <mirsella@protonmail.com>
2023-09-28 22:42:02 +0200

    Update .config/nvim/lua/config/keymaps.lua Add .config/nvim/lua/plugins/rust-tools.lua

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -11,22 +11,22 @@ vim.keymap.set("n", "<leader>sf", "<cmd>Telescope filetypes<cr>")
 local crates = require("crates")
 
 vim.keymap.set(
-	"n",
-	"<leader>cv",
-	crates.show_versions_popup,
-	{ silent = true, noremap = true, desc = "show crates versions" }
+  "n",
+  "<leader>cv",
+  crates.show_versions_popup,
+  { silent = true, noremap = true, desc = "show crates versions" }
 )
 vim.keymap.set(
-	"n",
-	"<leader>cf",
-	crates.show_features_popup,
-	{ silent = true, noremap = true, desc = "show crates features" }
+  "n",
+  "<leader>cf",
+  crates.show_features_popup,
+  { silent = true, noremap = true, desc = "show crates features" }
 )
 vim.keymap.set(
-	"n",
-	"<leader>cd",
-	crates.show_dependencies_popup,
-	{ silent = true, noremap = true, desc = "show crates dependencies" }
+  "n",
+  "<leader>cd",
+  crates.show_dependencies_popup,
+  { silent = true, noremap = true, desc = "show crates dependencies" }
 )
 
 vim.keymap.set("n", "<leader>cu", crates.update_crate, { silent = true, noremap = true, desc = "update crate" })
@@ -35,34 +35,34 @@ vim.keymap.set("n", "<leader>cU", crates.upgrade_crate, { silent = true, noremap
 vim.keymap.set("v", "<leader>cU", crates.upgrade_crates, { silent = true, noremap = true, desc = "upgrade crates" })
 
 vim.keymap.set(
-	"n",
-	"<leader>ce",
-	crates.expand_plain_crate_to_inline_table,
-	{ silent = true, noremap = true, desc = "expand crate to inline table" }
+  "n",
+  "<leader>ce",
+  crates.expand_plain_crate_to_inline_table,
+  { silent = true, noremap = true, desc = "expand crate to inline table" }
 )
 vim.keymap.set(
-	"n",
-	"<leader>cE",
-	crates.extract_crate_into_table,
-	{ silent = true, noremap = true, desc = "expand crate into table" }
+  "n",
+  "<leader>cE",
+  crates.extract_crate_into_table,
+  { silent = true, noremap = true, desc = "expand crate into table" }
 )
 
 vim.keymap.set("n", "<leader>cH", crates.open_homepage, { silent = true, noremap = true, desc = "open crate homepage" })
 vim.keymap.set(
-	"n",
-	"<leader>cR",
-	crates.open_repository,
-	{ silent = true, noremap = true, desc = "open crate repository" }
+  "n",
+  "<leader>cR",
+  crates.open_repository,
+  { silent = true, noremap = true, desc = "open crate repository" }
 )
 vim.keymap.set(
-	"n",
-	"<leader>cD",
-	crates.open_documentation,
-	{ silent = true, noremap = true, desc = "open crate homepage" }
+  "n",
+  "<leader>cD",
+  crates.open_documentation,
+  { silent = true, noremap = true, desc = "open crate homepage" }
 )
 vim.keymap.set(
-	"n",
-	"<leader>cC",
-	crates.open_crates_io,
-	{ silent = true, noremap = true, desc = "open crate on crates.io" }
+  "n",
+  "<leader>cC",
+  crates.open_crates_io,
+  { silent = true, noremap = true, desc = "open crate on crates.io" }
 )
