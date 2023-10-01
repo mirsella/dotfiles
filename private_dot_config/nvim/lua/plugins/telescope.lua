bc052c62ee82153c64a2a609e2a092ae315cb6de
mirsella <mirsella@protonmail.com>
2023-10-01 18:01:58 +0200

    Update .config/nvim/lua/plugins/telescope.lua Add .config/nvim/lua/plugins/undotree.lua

--- private_dot_config/nvim/lua/plugins/telescope.lua
+++ private_dot_config/nvim/lua/plugins/telescope.lua
@@ -1,20 +1,22 @@
 local Util = require("lazyvim.util")
 
 return {
-  "nvim-telescope/telescope.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "debugloop/telescope-undo.nvim",
-  },
+  -- "nvim-telescope/telescope.nvim",
+  -- dependencies = {
+  --   "nvim-lua/plenary.nvim",
+  --   "debugloop/telescope-undo.nvim",
+  -- },
   keys = {
-    { "<leader>uu", "<cmd>Telescope undo<cr>", desc = "Telescope undo" },
+    -- { "<leader>uu", "<cmd>Telescope undo<cr>", desc = "Telescope undo" },
 
-    { "<leader><space>", Util.telescope("files", { hidden = true }), desc = "Find Files (root dir)" },
+    -- { "<leader><space>", Util.telescope("files", { hidden = true }), desc = "Find Files (root dir)" },
+    -- { "<leader>ff", Util.telescope("files", { hidden = true }), desc = "Find Files (root dir)" },
+    -- { "<leader>fF", Util.telescope("files", { cwd = false, hidden = true }), desc = "Find Files (cwd)" },
+    { "<leader><space>", Util.telescope("files", { cwd = false, hidden = true }), desc = "Find Files (root dir)" },
     { "<leader>ff", Util.telescope("files", { hidden = true }), desc = "Find Files (root dir)" },
-    { "<leader>fF", Util.telescope("files", { cwd = false, hidden = true }), desc = "Find Files (cwd)" },
   },
   config = function()
     local telescope = require("telescope")
-    telescope.load_extension("undo")
+    -- telescope.load_extension("undo")
   end,
 }
