5c83f8069b4c0aba5843e14d17f3b9187fa462d4
mirsella <mirsella@protonmail.com>
2023-10-02 13:10:43 +0200

    Update .config/nvim/lua/plugins/telescope.lua

--- private_dot_config/nvim/lua/plugins/telescope.lua
+++ private_dot_config/nvim/lua/plugins/telescope.lua
@@ -1,22 +1,20 @@
 local Util = require("lazyvim.util")
 
 return {
-  -- "nvim-telescope/telescope.nvim",
-  -- dependencies = {
-  --   "nvim-lua/plenary.nvim",
-  --   "debugloop/telescope-undo.nvim",
-  -- },
-  keys = {
-    -- { "<leader>uu", "<cmd>Telescope undo<cr>", desc = "Telescope undo" },
+	-- "nvim-telescope/telescope.nvim",
+	-- dependencies = {
+	--   "nvim-lua/plenary.nvim",
+	--   "debugloop/telescope-undo.nvim",
+	-- },
+	keys = {
+		-- { "<leader>uu", "<cmd>Telescope undo<cr>", desc = "Telescope undo" },
 
-    -- { "<leader><space>", Util.telescope("files", { hidden = true }), desc = "Find Files (root dir)" },
-    -- { "<leader>ff", Util.telescope("files", { hidden = true }), desc = "Find Files (root dir)" },
-    -- { "<leader>fF", Util.telescope("files", { cwd = false, hidden = true }), desc = "Find Files (cwd)" },
-    { "<leader><space>", Util.telescope("files", { cwd = false, hidden = true }), desc = "Find Files (root dir)" },
-    { "<leader>ff", Util.telescope("files", { hidden = true }), desc = "Find Files (root dir)" },
-  },
-  config = function()
-    local telescope = require("telescope")
-    -- telescope.load_extension("undo")
-  end,
+		-- i don't know why but they show a error
+		-- { "<leader><space>", Util.telescope("files", { cwd = false, hidden = true }), desc = "Find Files (cwd)" },
+		-- { "<leader>ff", Util.telescope("files", { hidden = true }), desc = "Find Files (root dir)" },
+	},
+	config = function()
+		local telescope = require("telescope")
+		-- telescope.load_extension("undo")
+	end,
 }
