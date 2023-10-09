736a6dbb5dab3fe6942efff9f8ad61b78a260bcf
mirsella <mirsella@protonmail.com>
2023-10-09 19:36:18 +0200

    Update .config/zsh/lib/main/hostname.zsh

--- private_dot_config/zsh/lib/main/executable_hostname.zsh
+++ private_dot_config/zsh/lib/main/executable_hostname.zsh
@@ -1,12 +1,6 @@
-alias nvidia-settings='nvidia-settings --config="$XDG_CONFIG_HOME"/nvidia/settings'
-clip() { xclip -in -selection clipboard < "${@:-/dev/stdin}"; }
-clipp() { xclip -out -selection clipboard; }
 alias rmf='s /bin/rm -rf '
-# alias rm='s rmtrash -rf '
 alias rm='trash -rf'
 export PATH="$PATH:$HOME/dev/bin"
 hash -d d=/run/media/mirsella/ssd/documents.git/
 hash -d s=/run/media/mirsella/ssd/
 hash -d w=/run/media/mirsella/windows/Users/mirsella/
-
-export ZSH_SYSTEM_CLIPBOARD_METHOD='xcp'
