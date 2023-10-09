516dba1e66d33d94198a43b0ad84f0d9489b33c2
mirsella <mirsella@protonmail.com>
2023-10-09 19:19:45 +0200

    Update .chezmoidata.toml Update .chezmoiexternal.toml Update .config/zsh/lib/main/hostname.zsh

--- private_dot_config/zsh/lib/main/executable_hostname.zsh
+++ private_dot_config/zsh/lib/main/executable_hostname.zsh
@@ -4,7 +4,6 @@ clipp() { xclip -out -selection clipboard; }
 alias rmf='s /bin/rm -rf '
 # alias rm='s rmtrash -rf '
 alias rm='trash -rf'
-alias autoxrandr='xrandr --auto && xrandr --output DVI-D-0 --mode 1280x1024 --left-of HDMI-0 --mode 1920x1080 --rate 144'
 export PATH="$PATH:$HOME/dev/bin"
 hash -d d=/run/media/mirsella/ssd/documents.git/
 hash -d s=/run/media/mirsella/ssd/
