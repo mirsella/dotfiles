b6b538355e5c24811f1dfbcc975363ff2380039b
mirsella <mirsella@protonmail.com>
2023-11-06 16:13:58 +0100

    Update .config/zsh/lib/main/hostname.zsh

--- private_dot_config/zsh/lib/main/executable_hostname.zsh
+++ private_dot_config/zsh/lib/main/executable_hostname.zsh
@@ -5,3 +5,5 @@ export PATH="$PATH:$HOME/dev/bin"
 hash -d d=/run/media/mirsella/ssd/documents.git/
 hash -d s=/run/media/mirsella/ssd/
 hash -d w=/run/media/mirsella/windows/Users/mirsella/
+alias clip='wl-copy'
+alias clipp='wl-paste'
