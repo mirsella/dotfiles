f179a3a1ec475fc497ffa7fb3684fab7799cf33e
mirsella <mirsella@protonmail.com>
2023-11-28 18:32:03 +0100

    Update .config/nvim/lua/config/keymaps.lua Update .config/nvim/lua/plugins/nvim-lspconfig.lua

--- private_dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ private_dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,8 +1,8 @@
 return {
-	"neovim/nvim-lspconfig",
-	opts = {
-		inlay_hints = {
-			enabled = true,
-		},
-	},
+  "neovim/nvim-lspconfig",
+  opts = {
+    inlay_hints = {
+      enabled = true,
+    },
+  },
 }
