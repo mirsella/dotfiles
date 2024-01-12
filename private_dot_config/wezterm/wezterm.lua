adab2a7f8ba138a53ce5124d1e8b25565556e0c1
mirsella <mirsella@protonmail.com>
2024-01-12 14:22:41 +0100

    Update .config/wezterm/wezterm.lua

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 local config = wezterm.config_builder()
 
 config.colors = require("rose-pine/lua/rose-pine-moon").colors()
@@ -12,8 +13,17 @@ config.window_padding = {
 	top = 0,
 	bottom = 0,
 }
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+
 config.hyperlink_rules = wezterm.default_hyperlink_rules()
 config.scrollback_lines = 10000
 config.quick_select_alphabet = "colemak"
+
+-- start maximized
+wezterm.on("gui-startup", function(cmd)
+	local _, _, window = wezterm.mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
 
 return config
