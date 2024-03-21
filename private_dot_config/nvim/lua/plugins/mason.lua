677983508a7ba3ef1300c1aa5ad0b1a0de1244df
mirsella <mirsella@protonmail.com>
2024-03-21 23:06:38 +0100

    Add .config/nvim/lua/plugins/mason.lua

---
+++ private_dot_config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,26 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "stylua",
+      "shellcheck",
+      "vue-language-server",
+      "clangd",
+      "dockerfile-language-server",
+      "docker-compose-language-service",
+      "emmet-language-server",
+      "json-lsp",
+      "lua-language-server",
+      "markdowlint",
+      "marksman",
+      "prettier",
+      "pyright",
+      "rust-analyzer",
+      "shfmt",
+      "tailwind-language-server",
+      "taplo",
+      "typescript-language-server",
+      "yaml-language-server",
+    },
+  },
+}
