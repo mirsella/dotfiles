9a6140372f21b01dbd697f0749a033f0b0e3ee7d
mirsella <mirsella@protonmail.com>
2024-04-13 23:49:04 +0200

    Update .config/nvim/lua/plugins/neo-tree.lua

--- private_dot_config/nvim/lua/plugins/neo-tree.lua
+++ private_dot_config/nvim/lua/plugins/neo-tree.lua
@@ -1,9 +1,17 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	opts = {
-		close_if_last_window = true,
-		filesystem = {
-			hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
-		},
-	},
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    close_if_last_window = true,
+    filesystem = {
+      hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
+      commands = {
+        -- Override delete to use trash instead of rm
+        delete = function(state)
+          local path = state.tree:get_node().path
+          vim.fn.system({ "trash", vim.fn.fnameescape(path) })
+          require("neo-tree.sources.manager").refresh(state.name)
+        end,
+      },
+    },
+  },
 }
