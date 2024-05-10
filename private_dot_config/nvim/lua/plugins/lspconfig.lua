39a4fffdb7861bd252b3796083283cc7385420fe
mirsella <mirsella@protonmail.com>
2024-05-10 11:28:51 +0200

    Update .config/nvim/lua/plugins/lspconfig.lua Add .config/nvim/lua/plugins/oil.lua

--- private_dot_config/nvim/lua/plugins/lspconfig.lua
+++ private_dot_config/nvim/lua/plugins/lspconfig.lua
@@ -1,24 +1,8 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    inlay_hints = {
-      enabled = true,
-    },
-    -- for vue-language-server version 2
-    servers = {
-      tsserver = {
-        init_options = {
-          plugins = {
-            {
-              name = "@vue/typescript-plugin",
-              location = "/usr/lib/node_modules/@vue/typescript-plugin",
-              languages = { "javascript", "typescript", "vue" },
-            },
-          },
-        },
-        filetypes = { "typescript", "javascript", "vue" },
-      },
-      volar = {},
-    },
-  },
+	"neovim/nvim-lspconfig",
+	opts = {
+		inlay_hints = {
+			enabled = true,
+		},
+	},
 }
