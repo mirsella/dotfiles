6fd06f3fd861e8657374195f186fbee66b938166
mirsella <mirsella@protonmail.com>
2024-05-29 00:17:53 +0200

    Update .config/nvim/lua/config/lazy.lua Update .config/nvim/lua/config/options.lua

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.pumblend = 0 -- opaque completion menu
+-- vim.opt.pumblend = 0 -- opaque completion menu
 vim.opt.softtabstop = 2
 
 vim.opt.foldmethod = "expr"
