a81dd5a26ac2ac2ccade7dab178632c46212a328
mirsella <mirsella@protonmail.com>
2024-05-29 00:24:51 +0200

    Update .config/nvim/lua/plugins/mason.lua Update .config/nvim/lua/plugins/treesitter.lua

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -2,26 +2,5 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
     auto_install = true,
-    opts = {
-      ensure_installed = {
-        "bash",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
-        "rust",
-        "vue",
-        "toml",
-      },
-    },
   },
 }
