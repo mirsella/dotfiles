b280cdff8c539103be220e6ab7e8f7ee126c994e
mirsella <mirsella@protonmail.com>
2024-06-10 10:27:31 +0200

    gamp quick push

--- run_once_after_install-exportxdg.sh
+++ run_once_after_install-exportxdg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-if ! hostnamectl hostname | grep 42paris; then
+if hostnamectl hostname | grep "main|laptop|raspberrypi|tosh|unowhy"; then
 	echo 'source "$HOME"/.config/zsh/exportxdg' | sudo tee /etc/profile.d/exportxdg.sh
 fi
