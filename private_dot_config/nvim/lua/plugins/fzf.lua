025db59c652c5a91574f2bf92ae3bd3630d7c91a
mirsella <mirsella@protonmail.com>
2024-06-13 23:51:19 +0200

    Update .config/nvim/lua/plugins/fzf.lua

--- private_dot_config/nvim/lua/plugins/fzf.lua
+++ private_dot_config/nvim/lua/plugins/fzf.lua
@@ -1,10 +1,3 @@
-local function buf_dir()
-  local bufnum = vim.api.nvim_get_current_buf()
-  local fullpath = vim.api.nvim_buf_get_name(bufnum)
-  local basedir = vim.fn.fnamemodify(fullpath, ":p:h")
-  return basedir
-end
-
 return {
   {
     "ibhagwan/fzf-lua",
@@ -19,7 +12,7 @@ return {
       {
         "<leader>fF",
         function()
-          require("fzf-lua").files({ cwd = buf_dir() })
+          require("fzf-lua").files({ cwd = vim.fn.expand("%:p:h") })
         end,
         desc = "Find Files (Buffer Dir)",
       },
