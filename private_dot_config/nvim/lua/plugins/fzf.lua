17f1e529a631590c0e1ffeb140449c7c8bc534b1
mirsella <mirsella@protonmail.com>
2024-06-14 19:02:40 +0200

    Update .config/nvim/lua/plugins/fzf.lua

--- private_dot_config/nvim/lua/plugins/fzf.lua
+++ private_dot_config/nvim/lua/plugins/fzf.lua
@@ -1,25 +1,21 @@
 return {
-  {
-    "ibhagwan/fzf-lua",
-    keys = {
-      {
-        "<leader>ff",
-        function()
-          require("fzf-lua").files()
-        end,
-        desc = "Find Files (Root Dir)",
-      },
-      {
-        "<leader>fF",
-        function()
-          require("fzf-lua").files({ cwd = vim.fn.expand("%:p:h") })
-        end,
-        desc = "Find Files (Buffer Dir)",
-      },
-    },
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    enabled = false,
-  },
+	{
+		"ibhagwan/fzf-lua",
+		keys = {
+			{
+				"<leader>ff",
+				function()
+					require("fzf-lua").files()
+				end,
+				desc = "Find Files (Root Dir)",
+			},
+			{
+				"<leader>fF",
+				function()
+					require("fzf-lua").files({ cwd = vim.fn.expand("%:p:h") })
+				end,
+				desc = "Find Files (Buffer Dir)",
+			},
+		},
+	},
 }
