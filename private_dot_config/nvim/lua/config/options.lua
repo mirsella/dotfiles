b403c24a8cc47b51039d8886a4668faf0ced8c74
mirsella <mirsella@protonmail.com>
2024-06-14 18:17:37 +0200

    Update .config/nvim/lazyvim.json Update .config/nvim/lua/config/options.lua Update .config/nvim/lua/plugins/lspconfig.lua

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -4,8 +4,10 @@
 
 -- vim.opt.pumblend = 0 -- opaque completion menu
 -- vim.opt.softtabstop = 2
-
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldlevel = 99
 vim.opt.spelllang = "fr"
+
+vim.g.lazyvim_rust_diagnostics = "bacon-ls"
+vim.g.lazyvim_picker = "fzf"
--- private_dot_config/nvim/lua/plugins/lspconfig.lua
+++ private_dot_config/nvim/lua/plugins/lspconfig.lua
@@ -1,23 +1,23 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    inlay_hints = {
-      enabled = true,
-    },
-    setup = {
-      -- fix warning with rustaceans.nvim https://github.com/mrcjkb/rustaceanvim/blob/master/doc/mason.txt
-      rust_analyzer = function()
-        return true
-      end,
-      clangd = function(_, opts)
-        opts.capabilities.offsetEncoding = { "utf-16" }
-      end,
-    },
-  },
-  init = function()
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    -- disable a keymap
-    keys[#keys + 1] = { "<leader>cc", false }
-    keys[#keys + 1] = { "<leader>cC", false }
-  end,
+	"neovim/nvim-lspconfig",
+	opts = {
+		inlay_hints = {
+			enabled = true,
+		},
+		setup = {
+			-- fix warning with rustaceans.nvim https://github.com/mrcjkb/rustaceanvim/blob/master/doc/mason.txt
+			rust_analyzer = function()
+				return true
+			end,
+			clangd = function(_, opts)
+				opts.capabilities.offsetEncoding = { "utf-16" }
+			end,
+		},
+	},
+	init = function()
+		local keys = require("lazyvim.plugins.lsp.keymaps").get()
+		-- disable a keymap
+		keys[#keys + 1] = { "<leader>cc", false }
+		keys[#keys + 1] = { "<leader>cC", false }
+	end,
 }
