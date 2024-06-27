5324c3535802165d2542cc068804891c10befcb8
mirsella <mirsella@protonmail.com>
2024-06-27 23:38:34 +0200

    Update .config/nvim/lua/config/options.lua

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -9,5 +9,5 @@ vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldlevel = 99
 vim.opt.spelllang = "fr"
 
-vim.g.lazyvim_rust_diagnostics = "bacon-ls"
+-- vim.g.lazyvim_rust_diagnostics = "bacon-ls"
 vim.g.lazyvim_picker = "fzf"
