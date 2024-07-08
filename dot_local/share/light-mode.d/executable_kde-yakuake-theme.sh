d9f24b4554f58b8c97caffbf199c678067210762
mirsella <mirsella@protonmail.com>
2024-07-08 18:24:06 +0200

    Update .local/share/dark-mode.d/kde-yakuake-theme.sh Update .local/share/light-mode.d/kde-yakuake-theme.sh

--- dot_local/share/light-mode.d/executable_kde-yakuake-theme.sh
+++ dot_local/share/light-mode.d/executable_kde-yakuake-theme.sh
@@ -8,11 +8,11 @@
 # if there are multiple tabs in one of the Konsole instances.
 # Reference: https://docs.kde.org/stable5/en/konsole/konsole/konsole.pdf
 
-PROFILE='Breath-light'
+PROFILE='Light'
 # get number of sessions running within Yakuake
 SESSIONIDS=$(qdbus org.kde.yakuake /Sessions org.freedesktop.DBus.Introspectable.Introspect | grep -o '<node name="[0-9]\+"/>' | grep -o '[0-9]\+')
 for ID in $SESSIONIDS; do
 	# change profile through dbus message
-    qdbus org.kde.yakuake /Sessions/$ID setProfile "$PROFILE"
-    qdbus org.kde.yakuake /Windows/$ID setDefaultProfile "$PROFILE"
+	qdbus org.kde.yakuake /Sessions/"$ID" setProfile "$PROFILE"
+	qdbus org.kde.yakuake /Windows/"$ID" setDefaultProfile "$PROFILE"
 done
