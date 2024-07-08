d97e4a40f900c87b02ed94a5518f4c2c291c2d56
mirsella <mirsella@protonmail.com>
2024-07-08 18:21:08 +0200

    Add .local/share/dark-mode.d/desktop-notification.sh Add .local/share/dark-mode.d/gtk3-theme.sh Add .local/share/dark-mode.d/kde-global-theme.sh Add .local/share/dark-mode.d/kde-gtk-theme.sh Add .local/share/dark-mode.d/kde-konsole-theme.sh Add .local/share/dark-mode.d/kde-yakuake-theme.sh Add .local/share/light-mode.d/desktop-notification.sh Add .local/share/light-mode.d/gtk3-theme.sh Add .local/share/light-mode.d/kde-global-theme.sh Add .local/share/light-mode.d/kde-gtk-theme.sh Add .local/share/light-mode.d/kde-konsole-theme.sh Add .local/share/light-mode.d/kde-yakuake-theme.sh Remove etc/systemd/system/darkman.service

---
+++ dot_local/share/light-mode.d/executable_kde-yakuake-theme.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Yakuake is a drop-down terminal emulator based on KDE Konsole technology.
+# KDE's default terminal emulator supports profiles, you can create one in
+# Settings > Manage Profiles. You can select a dark or light theme in
+# Appearance > Color scheme and font. The following script iterates over all
+# instances of Konsole und changes the profile of all sessions. This is necessary,
+# if there are multiple tabs in one of the Konsole instances.
+# Reference: https://docs.kde.org/stable5/en/konsole/konsole/konsole.pdf
+
+PROFILE='Breath-light'
+# get number of sessions running within Yakuake
+SESSIONIDS=$(qdbus org.kde.yakuake /Sessions org.freedesktop.DBus.Introspectable.Introspect | grep -o '<node name="[0-9]\+"/>' | grep -o '[0-9]\+')
+for ID in $SESSIONIDS; do
+	# change profile through dbus message
+    qdbus org.kde.yakuake /Sessions/$ID setProfile "$PROFILE"
+    qdbus org.kde.yakuake /Windows/$ID setDefaultProfile "$PROFILE"
+done
