ea4e2566bd7852aa63878161759f2a6efef41eb0
mirsella <mirsella@protonmail.com>
2024-07-13 19:35:32 +0200

    Update .config/nvim/lazy-lock.json Update .config/wezterm/wezterm.lua

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,30 +1,21 @@
 local wezterm = require("wezterm")
-local rose_pine = require("lua/rose-pine").main
 local config = wezterm.config_builder()
+local rose_pine = require("lua/rose-pine")
 
--- config.colors = theme.colors()
--- config.window_frame = theme.window_frame()
-
-function scheme_for_appearance(appearance)
-	if appearance:find("Dark") then
-		return "Builtin Solarized Dark"
-	else
-		return "Builtin Solarized Light"
-	end
-end
-
-wezterm.on("window-config-reloaded", function(window, pane)
+wezterm.on("update-status", function(window)
 	local overrides = window:get_config_overrides() or {}
-	local appearance = window:get_appearance()
-	local colors = appearance:find("Dark") and rose_pine.moon or rose_pine.dawn
-	if overrides.colors ~= colors then
-		overrides.colors = colors
+	local appearance = wezterm.gui.get_appearance()
+	local scheme = appearance:find("Dark") and rose_pine.moon or rose_pine.dawn
+	if overrides.colors ~= scheme then
+		overrides.colors = scheme.colors()
+		overrides.window_frame = scheme.window_frame()
 		window:set_config_overrides(overrides)
 	end
 end)
 
-config.enable_scroll_bar = true
+-- config.enable_scroll_bar = true
 config.font_size = 20.0
+config.font = wezterm.font("JetBrains Mono")
 config.window_padding = {
 	left = 0,
 	right = 0,
@@ -32,14 +23,14 @@ config.window_padding = {
 	bottom = 0,
 }
 config.use_fancy_tab_bar = false
--- config.hide_tab_bar_if_only_one_tab = true
-
+config.hide_tab_bar_if_only_one_tab = true
 config.hyperlink_rules = wezterm.default_hyperlink_rules()
-config.scrollback_lines = 10000
+config.scrollback_lines = 100000
 config.quick_select_alphabet = "colemak"
 config.window_close_confirmation = "NeverPrompt"
 
--- start maximized
+-- config.window_decorations = "RESIZE"
+
 wezterm.on("gui-startup", function(cmd)
 	local _, _, window = wezterm.mux.spawn_window(cmd or {})
 	window:gui_window():maximize()
