40cd994d60d06db34a17fb09e550b9cd5f20a888
mirsella <mirsella@protonmail.com>
2024-07-13 16:04:59 +0200

    Update .config/wezterm/wezterm.lua

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,10 +1,28 @@
 local wezterm = require("wezterm")
-local theme = require("lua/rose-pine").main
+local rose_pine = require("lua/rose-pine").main
 local config = wezterm.config_builder()
 
-config.colors = theme.colors()
-config.window_frame = theme.window_frame()
-config.colors.scrollbar_thumb = "#5c6370"
+-- config.colors = theme.colors()
+-- config.window_frame = theme.window_frame()
+
+function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Builtin Solarized Dark"
+	else
+		return "Builtin Solarized Light"
+	end
+end
+
+wezterm.on("window-config-reloaded", function(window, pane)
+	local overrides = window:get_config_overrides() or {}
+	local appearance = window:get_appearance()
+	local colors = appearance:find("Dark") and rose_pine.moon or rose_pine.dawn
+	if overrides.colors ~= colors then
+		overrides.colors = colors
+		window:set_config_overrides(overrides)
+	end
+end)
+
 config.enable_scroll_bar = true
 config.font_size = 20.0
 config.window_padding = {
@@ -13,8 +31,8 @@ config.window_padding = {
 	top = 0,
 	bottom = 0,
 }
--- config.use_fancy_tab_bar = false
-config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+-- config.hide_tab_bar_if_only_one_tab = true
 
 config.hyperlink_rules = wezterm.default_hyperlink_rules()
 config.scrollback_lines = 10000
