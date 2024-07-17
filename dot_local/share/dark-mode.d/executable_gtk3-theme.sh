328b091668f82c4af9177fe3ec3089cca793947e
mirsella <mirsella@protonmail.com>
2024-07-17 22:26:21 +0200

    Update .local/share/dark-mode.d/gtk3-theme.sh Update .local/share/light-mode.d/gtk3-theme.sh Update .config/zsh/.zshrc Update .config/zsh/exportxdg

--- dot_local/share/dark-mode.d/executable_gtk3-theme.sh
+++ dot_local/share/dark-mode.d/executable_gtk3-theme.sh
@@ -4,3 +4,4 @@
 # "Darker" is actually LESS DARK than "Dark".
 
 gsettings set org.gnome.desktop.interface gtk-theme Arc-Dark
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark
--- dot_local/share/light-mode.d/executable_gtk3-theme.sh
+++ dot_local/share/light-mode.d/executable_gtk3-theme.sh
@@ -4,3 +4,4 @@
 # "Darker" is actually LESS DARK than "Dark".
 
 gsettings set org.gnome.desktop.interface gtk-theme Arc-Darker
+gsettings set org.gnome.desktop.interface color-scheme prefer-light
