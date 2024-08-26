9aeb42d2e60f5e7739a55e3cde971868ed2544a2
mirsella <mirsella@protonmail.com>
2024-08-26 12:21:05 +0200

    Update .config/nvim/lazy-lock.json Update .config/zsh/lib/42/hostname.zsh

--- private_dot_config/zsh/lib/42/executable_hostname.zsh
+++ private_dot_config/zsh/lib/42/executable_hostname.zsh
@@ -1,13 +1,17 @@
 export PATH="$PATH:$HOME/sgoinfre/cargo/bin"
 export CARGO_HOME="$HOME/sgoinfre/cargo"
 export RUSTUP_HOME="$HOME/sgoinfre/rustup"
+export ANDROID_SDK_HOME="$HOME/sgoinfre/android"
+export ANDROID_SDK_ROOT="$HOME/sgoinfre/android"
+export ANDROID_HOME="$HOME/sgoinfre/android"
 export BROWSER="firefox-esr"
 alias rmf='/bin/rm -rf '
+alias android-studio='flatpak run --env=ANDROID_SDK_HOME="$HOME/sgoinfre/android" --env=ANDROID_SDK_ROOT="$HOME/sgoinfre/android" --env=ANDROID_HOME="$HOME/sgoinfre/android" com.google.AndroidStudio'
 alias V='echo -n z | xclip -selection clipboard'
 bak() { cp -r "${1}" "${1}.bak" }
 bakm() { mv "${1}" "${1}.bak" }
-clipp() { xclip -out -selection clipboard; }
-clip() { xclip -in -selection clipboard < "${@:-/dev/stdin}"; }
+clipp () { xclip -out -selection clipboard; }
+clip () { xclip -in -selection clipboard < "${@:-/dev/stdin}"; }
 
 # [ ! -d ~/sgoinfre/.cache ] && mkdir ~/sgoinfre/.cache
 # export XDG_CACHE_HOME="$HOME/sgoinfre/.cache"
