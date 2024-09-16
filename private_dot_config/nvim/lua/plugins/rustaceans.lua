38931d56bfb3434ea487413fe497dc56741cd411
mirsella <mirsella@protonmail.com>
2024-09-16 16:22:18 +0200

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/rustaceans.lua Update .config/kglobalshortcutsrc

--- private_dot_config/nvim/lua/plugins/rustaceans.lua
+++ private_dot_config/nvim/lua/plugins/rustaceans.lua
@@ -1,21 +1,22 @@
 return {
-  "mrcjkb/rustaceanvim",
-  keys = {
-    {
-      "<leader>cc",
-      function()
-        vim.cmd.RustLsp("openCargo")
-      end,
-      ft = "rust",
-      desc = "Open Cargo.toml",
-    },
-    {
-      "<leader>cC",
-      function()
-        vim.cmd.RustLsp("openDocs")
-      end,
-      ft = "rust",
-      desc = "Open docs.rs document for symbol"
-    },
-  },
+	"mrcjkb/rustaceanvim",
+	keys = {
+		{
+			"<leader>cc",
+			function()
+				vim.cmd.RustLsp("openCargo")
+			end,
+			ft = "rust",
+			desc = "Open Cargo.toml",
+		},
+		{
+			"<leader>cC",
+			function()
+				vim.cmd.RustLsp("openDocs")
+			end,
+			mode = { "n", "v" },
+			ft = { "rust", "markdown" },
+			desc = "Open docs.rs document for symbol",
+		},
+	},
 }
