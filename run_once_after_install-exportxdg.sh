26a4dced5620951a33e6eaa37b1076bdf63ada51
mirsella <mirsella@protonmail.com>
2024-10-09 16:19:12 +0200

    Update install-exportxdg.sh

--- run_once_after_install-exportxdg.sh
+++ run_once_after_install-exportxdg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-if hostnamectl hostname | grep "main|laptop|raspberrypi|tosh|unowhy"; then
-	echo 'source "$HOME"/.config/zsh/exportxdg' | sudo tee /etc/profile.d/exportxdg.sh
+if hostnamectl hostname | grep -Eq "main|laptop|raspberrypi|tosh|unowhy"; then
+	echo "source $HOME/.config/zsh/exportxdg" | sudo tee /etc/profile.d/exportxdg.sh
 fi
