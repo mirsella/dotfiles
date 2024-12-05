c184544a32cda4d131b6b60d83cbee8051de9380
mirsella <mirsella@protonmail.com>
2024-12-05 10:25:41 +0100

    Update .config/nvim/lua/plugins/lspconfig.lua

--- private_dot_config/nvim/lua/plugins/lspconfig.lua
+++ private_dot_config/nvim/lua/plugins/lspconfig.lua
@@ -4,15 +4,6 @@ return {
 		inlay_hints = {
 			enabled = true,
 		},
-		setup = {
-			-- fix warning with rustaceans.nvim https://github.com/mrcjkb/rustaceanvim/blob/master/doc/mason.txt
-			rust_analyzer = function()
-				return true
-			end,
-			clangd = function(_, opts)
-				opts.capabilities.offsetEncoding = { "utf-16" }
-			end,
-		},
 	},
 	init = function()
 		local keys = require("lazyvim.plugins.lsp.keymaps").get()
