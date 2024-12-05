49c3a1040cbf02f2042e08741cc67e382c3a272d
mirsella <mirsella@protonmail.com>
2024-12-05 17:13:21 +0100

    Update .config/nvim/lua/config/options.lua Update .config/nvim/lua/plugins/cmp.lua

--- private_dot_config/nvim/lua/plugins/cmp.lua
+++ private_dot_config/nvim/lua/plugins/cmp.lua
@@ -19,7 +19,7 @@ return {
 			{ "chrisgrieser/cmp-nerdfont" },
 		},
 		opts = {
-			trigger = { completion = { keyword_range = "full" } },
+			-- trigger = { completion = { keyword_range = "full" } },
 			fuzzy = { prebuilt_binaries = { download = true } },
 			sources = {
 				completion = {
