964c9bc1d34b2c4649c99dd82d2ac80ff3fa05c7
mirsella <mirsella@protonmail.com>
2025-05-07 00:11:10 +0200

    Update .config/nvim/lua/plugins/lspconfig.lua

--- private_dot_config/nvim/lua/plugins/lspconfig.lua
+++ private_dot_config/nvim/lua/plugins/lspconfig.lua
@@ -1,14 +1,16 @@
 return {
-	"neovim/nvim-lspconfig",
-	opts = {
-		inlay_hints = {
-			enabled = true,
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			inlay_hints = {
+				enabled = true,
+			},
 		},
+		init = function()
+			local keys = require("lazyvim.plugins.lsp.keymaps").get()
+			-- disable a keymap
+			keys[#keys + 1] = { "<leader>cc", false }
+			keys[#keys + 1] = { "<leader>cC", false }
+		end,
 	},
-	init = function()
-		local keys = require("lazyvim.plugins.lsp.keymaps").get()
-		-- disable a keymap
-		keys[#keys + 1] = { "<leader>cc", false }
-		keys[#keys + 1] = { "<leader>cC", false }
-	end,
 }
