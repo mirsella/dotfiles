ab9cae6e2a1b13cace7dc1cbe8aebbd4238adb6c
mirsella <mirsella@protonmail.com>
2025-09-25 18:42:02 +0200

    Remove .config/nvim/lua/plugins/tree-sitter-surrealdb.lua

--- private_dot_config/nvim/lua/plugins/tree-sitter-surrealdb.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"dariuscorvus/tree-sitter-surrealdb.nvim",
-	dependencies = { "nvim-treesitter/nvim-treesitter" },
-	event = { "BufRead *.surql" },
-	config = function()
-		require("tree-sitter-surrealdb").setup()
-	end,
-}
