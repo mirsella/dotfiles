2da04c98169c197f5e579af5212b0c096ccf1e95
mirsella <mirsella@protonmail.com>
2025-11-01 21:53:21 +0100

    Update .config/opencode/plugin/notify-send.ts

--- private_dot_config/opencode/plugin/notify-send.ts
+++ private_dot_config/opencode/plugin/notify-send.ts
@@ -11,7 +11,8 @@ export const NotificationPlugin: Plugin = async ({
 		event: async ({ event }) => {
 			// Send notification on session completion
 			if (event.type === "session.idle") {
-				await $`notify-send -a "opencode" "Session completed!"`;
+				// NOTE: temporarily disabled becuase of a bug, the event is fired 6 times and error: GDBus.Error:org.freedesktop.Notifications.Error.ExcessNotificationGeneration: Created too many similar notifications in quick succession
+				// await $`notify-send -a "opencode" "Session completed!"`;
 			}
 		},
 	};
