b9dfcd03e43554f243b3c530bde6c895a1112d8c
mirsella <mirsella@protonmail.com>
2026-08-10 08:32:26 +0200

    Add .config/nvim/lua/plugins/flash.lua

---
+++ private_dot_config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,6 @@
+return {
+	"folke/flash.nvim",
+	-- TODO: Delete this override to return to upstream main once PR #496 is merged.
+	url = "https://github.com/pedro757/flash.nvim.git",
+	branch = "fix/neovim-0.13-search-state",
+}
