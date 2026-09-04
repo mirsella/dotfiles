aed2333e485fd19cb79f360cb805cf323abb00e0
mirsella <mirsella@protonmail.com>
2026-09-04 11:37:15 +0200

    Remove .config/nvim/lua/plugins/auto-dark-mode.lua Remove .config/nvim/lua/plugins/colorizer.lua Remove .config/nvim/lua/plugins/lspconfig.lua Remove .config/nvim/lua/plugins/treesitter.lua

--- private_dot_config/nvim/lua/plugins/colorizer.lua
+++
@@ -1,3 +0,0 @@
-return {
-	"catgoose/nvim-colorizer.lua",
-}
--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"nvim-treesitter/nvim-treesitter",
-	event = function()
-		return { "LazyFile" }
-	end,
-	opts = {
-		auto_install = true,
-	},
-}
