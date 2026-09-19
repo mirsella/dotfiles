469374c88f1d79aa83a61cce952a14401afe0f79
mirsella <mirsella@protonmail.com>
2026-09-20 00:19:51 +0200

    computer-use-mcp gains bindgenHook for libclang

--- pkgs/computer-use-mcp/default.nix
+++ pkgs/computer-use-mcp/default.nix
@@ -8,7 +8,7 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-KzJgrBMb+RqeVSDLezFOhWoatKn3RtmS8YXO2PUrG6I=";
   };
   cargoHash = "sha256-+e4qxWg9WoeX9gxKlDlHr9zrzl5X2aqP4OqSjtz6GV4=";
-  nativeBuildInputs = [ pkg-config ];
+  nativeBuildInputs = [ pkg-config rustPlatform.bindgenHook ];
   buildInputs = [ wayland libxkbcommon glib pipewire ];
   doCheck = false;
 }
