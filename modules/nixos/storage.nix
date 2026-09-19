4f9ec136dd7d0e23fb756ab45fdef5cf66cf0ae6
mirsella <mirsella@protonmail.com>
2026-09-19 17:52:27 +0200

    udev extraRules is lines, not a list

--- modules/nixos/storage.nix
+++ modules/nixos/storage.nix
@@ -166,9 +166,9 @@ lib.mkMerge [
 
   # Genesys Logic hub autosuspends with delay 0 and takes the pool disks
   # off the bus with it (killed 6 SMART long tests). Keep it awake.
-  services.udev.extraRules = [
-    ''ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="05e3", ATTR{idProduct}=="0626", ATTR{power/control}="on"''
-  ];
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="05e3", ATTR{idProduct}=="0626", ATTR{power/control}="on"
+  '';
 
   systemd.services.hd-idle = {
     description = "Spin down idle tank HDDs";
