d7e24b81afbecad8bcdd2f9520db9cf4b9084a59
mirsella <mirsella@protonmail.com>
2026-09-20 18:56:47 +0200

    nextcloud mail via resend smtp

--- modules/nixos/nextcloud.nix
+++ modules/nixos/nextcloud.nix
@@ -9,6 +9,16 @@
     ];
   };
 
+  sops.secrets.nextcloud-resend = {
+    sopsFile = ../../secrets/nextcloud.yaml;
+    key = "resend_api_key";
+    owner = "nextcloud";
+    restartUnits = [
+      "nextcloud-setup.service"
+      "phpfpm-nextcloud.service"
+    ];
+  };
+
   services.nextcloud = {
     enable = true;
     hostName = "mirsella.mooo.com";
@@ -28,6 +38,14 @@
       overwriteprotocol = "https";
       overwritewebroot = "/nextcloud";
       "overwrite.cli.url" = "https://mirsella.mooo.com/nextcloud";
+      mail_smtpmode = "smtp";
+      mail_smtphost = "smtp.resend.com";
+      mail_smtpport = 465;
+      mail_smtpsecure = "ssl";
+      mail_smtpauth = true;
+      mail_smtpname = "resend";
+      mail_from_address = "noreply";
+      mail_domain = "voxride.com";
       enabledPreviewProviders = [
         "OC\\Preview\\BMP"
         "OC\\Preview\\GIF"
@@ -43,6 +61,7 @@
     extraApps = {
       inherit (config.services.nextcloud.package.packages.apps) memories;
     };
+    secrets.mail_smtppassword = config.sops.secrets.nextcloud-resend.path;
   };
 
   services.nginx.virtualHosts."mirsella.mooo.com".listen = [
