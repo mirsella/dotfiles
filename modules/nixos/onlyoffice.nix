d7156b54d8ca8bdbaec74666a39c0624b89e4062
mirsella <mirsella@protonmail.com>
2026-09-20 19:15:02 +0200

    onlyoffice nonce secret, nextcloud secrets refresh

--- modules/nixos/onlyoffice.nix
+++ modules/nixos/onlyoffice.nix
@@ -7,10 +7,22 @@
     restartUnits = [ "onlyoffice-documentserver.service" ];
   };
 
+  sops.secrets.onlyoffice-nonce = {
+    sopsFile = ../../secrets/nextcloud.yaml;
+    key = "onlyoffice_nonce";
+  };
+  sops.templates."onlyoffice-nonce.conf" = {
+    content = 'set $secure_link_secret "${config.sops.placeholder.onlyoffice-nonce}";';
+    owner = "root";
+    group = "onlyoffice";
+    mode = "0440";
+  };
+
   services.onlyoffice = {
     enable = true;
     port = 8000;
     allowLocalConnections = true;
     jwtSecretFile = config.sops.secrets.onlyoffice-jwt.path;
+    securityNonceFile = config.sops.templates."onlyoffice-nonce.conf".path;
   };
 }
